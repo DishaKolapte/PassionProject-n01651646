a3d3f490c1949c716a98ba1a70c3f90f9d72d1b4
Disha Kolapate <dishakolapate@gmail.com>
2024-06-08 02:45:20 -0400

    Updated Inquiry method migrations.

---
+++ PassionProject-n01651646/Migrations/202406080642403_Inquiry.cs
@@ -0,0 +1,22 @@
+﻿namespace PassionProject_n01651646.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Inquiry : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Inquiries", "PetName", c => c.String());
+            AddColumn("dbo.Inquiries", "Username", c => c.String());
+            DropColumn("dbo.Users", "Email");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Users", "Email", c => c.String());
+            DropColumn("dbo.Inquiries", "Username");
+            DropColumn("dbo.Inquiries", "PetName");
+        }
+    }
+}
